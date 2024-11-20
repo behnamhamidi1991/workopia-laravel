e98cfcdde83c04b01295d2680ac82fdf1b81bc4e
= <eslp91@hotmail.com>
2024-11-20 06:45:16 +0330

    first commit

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -29,12 +29,9 @@
             <i class="fa fa-gauge mr-1"></i> Dashboard
             
         </x-nav-link>
-        <a
-          href="{{url('/jobs/create')}}"
-          class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"
-        >
-          <i class="fa fa-edit"></i> Create Job
-        </a>
+   
+        <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+
       </nav>
       <button id="hamburger" class="text-white md:hidden flex items-center">
         <i class="fa fa-bars text-2xl"></i>
@@ -45,22 +42,32 @@
       id="mobile-menu"
       class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
     >
-      <a href="jobs.html" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-      <a href="saved-jobs.html" class="block px-4 py-2 hover:bg-blue-700"
-        >Saved Jobs</a
-      >
-      <a href="dashboard.html" class="block px-4 py-2 hover:bg-blue-700"
-        >Dashboard</a
-      >
-      <a href="login.html" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-      <a href="register.html" class="block px-4 py-2 hover:bg-blue-700"
-        >Register</a
-      >
-      <a
-        href="create-job.html"
-        class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black"
-      >
-        <i class="fa fa-edit"></i> Create Job
-      </a>
+    <x-nav-link url="/" :active="request()->is('/')" :mobile="true">
+      Home
+    </x-nav-link>
+
+    <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">
+      All Jobs
+  </x-nav-link>
+
+  <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">
+      Saved Jobs
+  </x-nav-link>
+
+  <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">
+      Login
+  </x-nav-link>
+
+  <x-nav-link url="/register" :active="request()->is('jobs/saved')" :mobile="true">
+      Register
+  </x-nav-link>
+
+  <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">
+      <i class="fa fa-gauge mr-1"></i> Dashboard
+      
+  </x-nav-link>
+  <x-button-link url="/jobs/create">
+    Create a Job
+  </x-button-link>
     </div>
   </header>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,13 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     @vite('resources/css/app.css')
     <title>{{$title ?? 'Workopia | Find and List Jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header />
+
+    @if(request()->is('/'))
+    <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
+
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
